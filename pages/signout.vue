fb331049b0627adbdc0e46c03b21f216ac5b4aa2
Kotyazya <tankikross@mail.ru>
2024-04-22 13:19:29 +0300

    Развернут тпроект на Наксте

---
+++ pages/signout.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Выйти из аккаунта?</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: "signout"
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
---
+++ pages/signup.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>РЕГИСТРАЦИЯ</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: "signup"
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
